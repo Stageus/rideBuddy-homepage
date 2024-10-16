1cd9ff230b01905bcc29e64ad48daeeb3879918d
plzcoboo <plzcobooo@gmail.com>
2024-10-16 15:39:54 +0900

    정규표현식 자료 추가

---
+++ src/assets/regex.js
@@ -0,0 +1,5 @@
+export const idRegex = /^[a-zA-Z]{1,20}$/; // 아이디: 1~20자의 영문 대소문자만 허용
+export const pwRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\*\&\^\@\!])[A-Za-z\d\*\&\^\@\!]{1,20}$/; // 비밀번호: 영대소문자, 숫자, 특수문자 포함, 최대 20자
+export const nameRegex = /^[가-힣]{1,5}$/; // 이름: 1~5자의 한글만 허용
+export const mailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // 이메일: 최대 30자, @ 포함 형식
+export const codeRegex = /^\d{6}$/; // 인증 코드: 6자리 숫자 고정
