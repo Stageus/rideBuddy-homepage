c3457522d6ad09fe0102b843d8796740d2ad5154
plzcoboo <plzcobooo@gmail.com>
2024-11-14 14:21:42 +0900

    프록시 우회

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/reverse-geocode': {
+        target: 'https://naveropenapi.apigw.ntruss.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/reverse-geocode/, '/map-reversegeocode/v2/gc'),
+        secure: false,
+      },
+    },
+  },
 })
